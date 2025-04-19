2b767d46b164393319a9c57ccbafd594602ca45b
fominykhpolina <polina.fominykh4@gmail.com>
2025-04-19 17:16:43 +0300

    HW11

--- src/com/basejava/webapp/MainConcurrency.java
+++ src/com/basejava/webapp/MainConcurrency.java
@@ -1,5 +1,99 @@
 package com.basejava.webapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainConcurrency {
-    public static void main (String[] main)
+    public static final int THREADS_NUMBER = 10000;
+    private int counter;
+    private static final Object LOCK = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+                throw new IllegalStateException();
+            }
+        };
+        thread0.start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+            }
+
+            private void inc() {
+                synchronized (this) {
+//                    counter++;
+                }
+            }
+        }).start();
+
+        System.out.println(thread0.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(mainConcurrency.counter);
+
+        final String firstLock = "FirstLock";
+        final String secondLock = "SecondLock";
+        deadLock(firstLock, secondLock);
+        deadLock(secondLock, firstLock);
+    }
+
+    private static void deadLock(final Object firstLock, final Object secondLock) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Waiting " + firstLock);
+                synchronized (firstLock) {
+                    System.out.println("Holding " + firstLock);
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Waiting " + secondLock);
+                    synchronized (secondLock) {
+                        System.out.println("Holding " + secondLock);
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    private synchronized void inc() {
+//        synchronized (this) {
+//        synchronized (MainConcurrency.class) {
+        counter++;
+//                wait();
+//                readFile
+//                ...
+//        }
+    }
 }
