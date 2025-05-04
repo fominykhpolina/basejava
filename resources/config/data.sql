8fc932a1bf41eb5197f671ef4e128e350d1d68f4
fominykhpolina <polina.fominykh4@gmail.com>
2025-05-05 01:57:13 +0300

    Files

---
+++ resources/config/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO resume (uuid, full_name) VALUES
+  ('a1b2c3d4-5678-4abc-9012-abcdef123456', 'Name1'),
+  ('123e4567-e89b-12d3-a456-426614174000', 'Name2'),
+  ('145e4457-e89b-14r3-a236-426235254000', 'Name3');
